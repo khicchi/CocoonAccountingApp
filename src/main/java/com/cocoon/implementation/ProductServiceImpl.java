5d0d246f7bc1f98e6282cb9d7438c76c93d398a6
otto <otto.o@cybertekschool.com>
2022-02-07 14:08:23 +0300

    method implementation..

--- src/main/java/com/cocoon/implementation/ProductServiceImpl.java
+++ src/main/java/com/cocoon/implementation/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.cocoon.implementation;
 
 import com.cocoon.dto.ProductDTO;
 import com.cocoon.entity.Product;
+import com.cocoon.repository.ProductRepository;
 import com.cocoon.service.ProductService;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,17 @@ import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
         return null;
     }
 
--- src/main/java/com/cocoon/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cocoon.repository;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProjectRepository {
-}
