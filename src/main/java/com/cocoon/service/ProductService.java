64e1de4e8842c5e1dce81d46f172b6c2936e3b4c
otto <otto.o@cybertekschool.com>
2022-02-07 13:55:45 +0300

    Product Service is created.

---
+++ src/main/java/com/cocoon/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.cocoon.service;
+
+import com.cocoon.dto.ProductDTO;
+import com.cocoon.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<ProductDTO> getAllProducts();
+    void save(Product product);
+
+}
