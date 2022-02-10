add0329aef32c429c62abde5dac2a083be9b954b
otto <otto.o@cybertekschool.com>
2022-02-10 15:02:54 +0300

    product-edit changes..

--- src/main/java/com/cocoon/implementation/ProductServiceImpl.java
+++ src/main/java/com/cocoon/implementation/ProductServiceImpl.java
@@ -56,10 +56,6 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void update(ProductDTO productDTO) {
         Product product = mapperUtil.convert(productDTO, new Product());
-
-
-
-
         productRepository.save(product);
     }
 
