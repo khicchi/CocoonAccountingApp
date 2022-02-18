7e15a82a6a1746fd22d560f3bd8ea93f68b8f13f
owermaiden <ower.maiden@cybertekschool.com>
2022-02-18 10:42:37 +0300

    some bugs

--- src/main/java/com/cocoon/controller/PurchaseInvoiceController.java
+++ src/main/java/com/cocoon/controller/PurchaseInvoiceController.java
@@ -6,6 +6,7 @@ import com.cocoon.dto.InvoiceProductDTO;
 import com.cocoon.enums.CompanyType;
 import com.cocoon.enums.InvoiceType;
 import com.cocoon.exception.CocoonException;
+import com.cocoon.repository.ClientVendorRepo;
 import com.cocoon.service.ClientVendorService;
 import com.cocoon.service.InvoiceProductService;
 import com.cocoon.service.InvoiceService;
@@ -33,12 +34,14 @@ public class PurchaseInvoiceController {
     private final ProductService productService;
     private final InvoiceProductService invoiceProductService;
     private final ClientVendorService clientVendorService;
+    private final ClientVendorRepo clientVendorRepo;
 
-    public PurchaseInvoiceController(InvoiceService invoiceService, ProductService productService, InvoiceProductService invoiceProductService, ClientVendorService clientVendorService) {
+    public PurchaseInvoiceController(InvoiceService invoiceService, ProductService productService, InvoiceProductService invoiceProductService, ClientVendorService clientVendorService, ClientVendorRepo clientVendorRepo) {
         this.invoiceService = invoiceService;
         this.productService = productService;
         this.invoiceProductService = invoiceProductService;
         this.clientVendorService = clientVendorService;
+        this.clientVendorRepo = clientVendorRepo;
     }
 
     @GetMapping({"/list", "/list/{cancel}"})
@@ -63,7 +66,7 @@ public class PurchaseInvoiceController {
     public String purchaseInvoiceCreate(@RequestParam(required = false) Long id, Model model) throws CocoonException{
 
         if (id != null){
-            currentInvoiceDTO.setClientVendor(clientVendorService.findById(id));
+            currentInvoiceDTO.setClientVendor(clientVendorRepo.getById(id));
         }
         currentInvoiceDTO.setInvoiceNumber(invoiceService.getInvoiceNumber(InvoiceType.PURCHASE));
         currentInvoiceDTO.setInvoiceDate(LocalDate.now());
