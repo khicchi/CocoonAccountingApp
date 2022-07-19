d11f3a70d85537f7a9eaf5aa77a08acaaf6f119a
otto <otto.o@cybertekschool.com>
2022-07-20 01:14:28 +0300

    unit testing..

--- src/test/java/com/cocoon/implementation/ProductServiceImplTest.java
+++ src/test/java/com/cocoon/implementation/ProductServiceImplTest.java
@@ -1,26 +1,48 @@
 package com.cocoon.implementation;
 
+import com.cocoon.dto.ProductDTO;
+import com.cocoon.entity.Category;
+import com.cocoon.entity.Company;
 import com.cocoon.entity.Product;
+import com.cocoon.entity.State;
 import com.cocoon.enums.ProductStatus;
+import com.cocoon.enums.Unit;
+import com.cocoon.repository.InvoiceProductRepository;
 import com.cocoon.repository.ProductRepository;
+import com.cocoon.service.CompanyService;
 import com.cocoon.util.MapperUtil;
-import org.aspectj.lang.annotation.Before;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ContextConfiguration(classes = {ProductServiceImpl.class})
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
+
+    @MockBean
+    private CompanyService companyService;
+
 
     @Mock
     ProductRepository productRepository;
@@ -29,37 +51,46 @@ class ProductServiceImplTest {
     @InjectMocks
     ProductServiceImpl productService;
 
-    @Test
-    public void getAllProducts() {
-        List<Product> productList = new ArrayList<>();
-        Product product = new Product();
+    static Product product = new Product();
+    static Product product2 = new Product();
+    static ProductDTO productDTO = new ProductDTO();
+    static List<Product> productList = new ArrayList<>();
+
+    @BeforeAll
+    static void setUp() {
         product.setId(17L);
         product.setProductStatus(ProductStatus.ACTIVE);
+
+        product.setId(18L);
+        product.setProductStatus(ProductStatus.ACTIVE);
+
         productList.add(product);
+        productList.add(product2);
+    }
 
-        Product product1 = new Product();
-        productList.add(product1);
+    @Test
+    public void getAllProducts() {
 
-        Mockito.when(productRepository.findAll()).thenReturn(productList);
+        when(productRepository.findAll()).thenReturn(productList);
+        when(mapperUtil.convert(any(), (ProductDTO) any())).thenReturn(new ProductDTO());
 
+        //verify(productRepository).findAll();
+        //verify(mapperUtil).convert(any(), (ProductDTO) any());
         assertEquals(2, productService.getAllProducts().size());
 
     }
 
     @Test
     public void getProductStatusById() {
-        List<Product> productList = new ArrayList<>();
-        Product product = new Product();
-        product.setId(17L);
-        product.setProductStatus(ProductStatus.ACTIVE);
-        productList.add(product);
 
-        Product product1 = new Product();
-        productList.add(product1);
-
-        Mockito.when(productRepository.findById(product.getId())).thenReturn(java.util.Optional.of(product));
+        when(productRepository.findById(any())).thenReturn(Optional.of(product));
 
         assertEquals(ProductStatus.ACTIVE, productService.getProductStatusById(17L));
+
+    }
+
+    @Test
+    void getAllProductsByCompany() {
 
     }
 }
