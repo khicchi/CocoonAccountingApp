89f89c7979820f8211f4f778f48d850e85523672
otto <otto.o@cybertekschool.com>
2022-07-20 09:42:50 +0300

    unit testing..

--- src/test/java/com/cocoon/implementation/ProductServiceImplTest.java
+++ src/test/java/com/cocoon/implementation/ProductServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.cocoon.implementation;
 
+import com.cocoon.dto.CompanyDTO;
 import com.cocoon.dto.ProductDTO;
 import com.cocoon.entity.Category;
 import com.cocoon.entity.Company;
@@ -11,6 +12,7 @@ import com.cocoon.exception.NoSuchProductException;
 import com.cocoon.repository.InvoiceProductRepository;
 import com.cocoon.repository.ProductRepository;
 import com.cocoon.service.CompanyService;
+import com.cocoon.service.ProductService;
 import com.cocoon.util.MapperUtil;
 
 import java.time.LocalDate;
@@ -26,6 +28,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -41,33 +44,38 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
 
-    @MockBean
+    @Mock
     private CompanyService companyService;
-
-
     @Mock
-    ProductRepository productRepository;
+    private ProductService productService;
     @Mock
-    MapperUtil mapperUtil;
+    private ProductRepository productRepository;
+    @Mock
+    private MapperUtil mapperUtil;
     @InjectMocks
-    ProductServiceImpl productService;
+    private ProductServiceImpl productServiceImpl;
 
     static Product product = new Product();
     static Product product2 = new Product();
     static ProductDTO productDTO = new ProductDTO();
+    static ProductDTO productDTO2 = new ProductDTO();
     static List<Product> productList = new ArrayList<>();
-    static Company company = new Company();
+    static CompanyDTO companyDTO = new CompanyDTO();
 
     @BeforeAll
     static void setUp() {
         product.setId(17L);
         product.setProductStatus(ProductStatus.ACTIVE);
 
-        product.setId(18L);
-        product.setProductStatus(ProductStatus.ACTIVE);
+        product2.setId(18L);
+        product2.setProductStatus(ProductStatus.ACTIVE);
 
         productList.add(product);
         productList.add(product2);
+
+        companyDTO.setId(1L);
+        companyDTO.setTitle("Cyber");
+
     }
 
     @Test
@@ -76,10 +84,44 @@ class ProductServiceImplTest {
         when(productRepository.findAll()).thenReturn(productList);
         when(mapperUtil.convert(any(), (ProductDTO) any())).thenReturn(new ProductDTO());
 
-        //verify(productRepository).findAll();
-        //verify(mapperUtil).convert(any(), (ProductDTO) any());
-        assertEquals(2, productService.getAllProducts().size());
+        assertEquals(2, productServiceImpl.getAllProducts().size());
 
+    }
+
+    @Test
+    void getAllProductsByCompany() {
+
+        when(companyService.getCompanyByLoggedInUser()).thenReturn(companyDTO);
+        when(productRepository.findAllByCompanyId(any())).thenReturn(productList);
+        when(mapperUtil.convert(any(), (ProductDTO) any())).thenReturn(new ProductDTO());
+
+        assertEquals(2, productServiceImpl.getAllProductsByCompany().size());
+    }
+
+    @Test
+    void save() {
+
+        when(mapperUtil.convert(any(), (Product) any())).thenReturn(new Product());
+        when(companyService.getCompanyByLoggedInUser()).thenReturn(companyDTO);
+        when(productRepository.save(product)).thenReturn(product);
+
+        assertNotNull(mapperUtil.convert(any(), any()));
+        assertNotNull(companyService.getCompanyByLoggedInUser());
+        assertNotNull(productRepository.save(product));
+
+    }
+
+    @Test
+    void getProductById() {
+
+        when(productRepository.findById(product.getId())).thenReturn(Optional.ofNullable(product));
+        when(mapperUtil.convert(any(), (ProductDTO) any())).thenReturn(new ProductDTO());
+
+        assertNotNull(productServiceImpl.getProductById(product.getId()));
+    }
+
+    @Test
+    void update() {
     }
 
     @Test
@@ -88,14 +130,29 @@ class ProductServiceImplTest {
         when(productRepository.findById(any())).thenReturn(Optional.of(product));
         when(productRepository.findById(0L)).thenThrow(new NoSuchProductException());
 
-        assertEquals(ProductStatus.ACTIVE, productService.getProductStatusById(product.getId()));
-        assertNotEquals(ProductStatus.PASSIVE, productService.getProductStatusById(product2.getId()));
-        assertThrows(NoSuchProductException.class, () -> productService.getProductStatusById(0L));
+        assertEquals(ProductStatus.ACTIVE, productServiceImpl.getProductStatusById(product.getId()));
+        assertNotEquals(ProductStatus.PASSIVE, productServiceImpl.getProductStatusById(product2.getId()));
+        assertThrows(NoSuchProductException.class, () -> productServiceImpl.getProductStatusById(0L));
 
     }
 
     @Test
-    void getAllProductsByCompany() {
+    void getUnitById() {
+    }
 
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findProductsByCategoryId() {
+    }
+
+    @Test
+    void updateProductQuantity() {
+    }
+
+    @Test
+    void validateProductQuantity() {
     }
 }
