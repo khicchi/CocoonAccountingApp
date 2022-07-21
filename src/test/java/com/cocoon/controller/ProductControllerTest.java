9697d9d4b1c3858f214355d7c2bf4bb6eee03d22
otto <otto.o@cybertekschool.com>
2022-07-21 09:25:39 +0300

    integration testing..

--- src/test/java/com/cocoon/controller/ProductControllerTest.java
+++ src/test/java/com/cocoon/controller/ProductControllerTest.java
@@ -1,22 +1,57 @@
 package com.cocoon.controller;
 
+import com.cocoon.service.CategoryService;
+import com.cocoon.service.ProductService;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class ProductControllerTest {
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProductController productController;
+
+    @MockBean
+    private ProductService productService;
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    void getAllProducts() {
-
+    void testGetAllProducts() throws Exception {
+        when(productService.getAllProductsByCompany()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/product/list");
+        MockMvcBuilders.standaloneSetup(productController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("products"))
+                .andExpect(MockMvcResultMatchers.view().name("product/product-list"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("product/product-list"));
     }
+
 }
